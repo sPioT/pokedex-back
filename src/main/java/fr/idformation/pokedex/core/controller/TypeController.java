c6fce88046e27c2d5faaff0a84878ecc2157af12
Stéphane PIOTROWSKI <stephane.piotrowski@gmail.com>
2024-03-01 16:13:23 +0100

    Add comment on getAll()

--- src/main/java/fr/idformation/pokedex/core/controller/TypeController.java
+++ src/main/java/fr/idformation/pokedex/core/controller/TypeController.java
@@ -21,6 +21,9 @@ public class TypeController {
 	@Autowired
 	private ITypeService typeService;
 
+	/**
+	 * @return all types
+	 */
 	@GetMapping("/")
 	public List<TypeDTO> getAll() {
 		return TypeMapper.typesToDtos(typeService.getAll());
